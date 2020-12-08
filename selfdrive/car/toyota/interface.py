381fae87da0774049e9bebffd569fc5bab9c0a3d
d412k5t412 <36134180+d412k5t412@users.noreply.github.com>
2020-12-07 23:34:41 -0500

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -81,7 +81,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.init('indi')
         ret.lateralTuning.indi.innerLoopGain = 4.4
         ret.lateralTuning.indi.outerLoopGainBP = [20, 21, 25, 26]
-        ret.lateralTuning.indi.outerLoopGainV = [2.6, 3.0, 3.2, 3.8]
+        ret.lateralTuning.indi.outerLoopGainV = [3.0, 3.2, 3.6, 3.8]
         ret.lateralTuning.indi.timeConstant = 0.1 if ret.hasZss else 1.47
         ret.lateralTuning.indi.actuatorEffectiveness = 1.0
       else:
